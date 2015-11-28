3c7216b259cd10f7355ab52167e04767e33f61d0
Kevin S <kshen3778@gmail.com>
2015-11-28 10:50:00 +0000

    Finished connection

--- Client.java
+++ Client.java
@@ -4,21 +4,40 @@ public class Client
     private PrintWriter out;
     private BufferedReader in;
     
-    public Client(Socket s, PrintWriter pw, BufferedReader br)
+    public Client(Socket s) throws IOException
     {
         socket = s;
-        out = pw;
-        in = br;
+		out = new PrintWriter(s.getOutputStream(), true);
+		in = new BufferedReader(new InputStreamReader(s.getInputStream()));
     }
     
-    public void give(String str)
+    public void give(String str) throws IOException
     {
         out.println(str);
     }
     
-    public String get()
+    public String get() throws IOException
     {
         return in.readLine();
     }
+    
+    public static double angleFinder(double targetX, double targetY ){
+		double theta, angle;
+		theta = Math.atan2(targetY , targetX);
+		theta += Math.PI/360;
+		angle = Math.toDegrees(theta);
+		if (angle < 0) {
+	        angle += 360;
+	    }
+		return angle;
+	}
+	
+	public static double targetDistance(double targetX, double targetY ){
+		double distance;
+		distance = Math.sqrt((targetX * targetX) + (targetY * targetY));
+		return distance;
+	}
+    
+    
     
 }
--- Server.java
+++ Server.java
@@ -6,81 +6,35 @@ import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
-class Server
+public class Server
 {    
 	ServerSocket server;
 	
-    public static void main(String args[]) throws Exception       
+    public static void main(String[] args) throws Exception       
     {          
         try {
 			server = new ServerSocket(8080);
-
+			listen();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
     } 
     
-    public void listen()
+    public void listen() throws IOException
     {
-    	ArrayList<Socket> clients = new ArrayList<Socket>();
+    	Client assassin;
+    	Client victim;
     	
-    	ActionListener actionListener = new ActionListener(){
-    		//After 30 seconds wait time
-    		Socket client = server.accept();
-			PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-			System.out.println(client);
-    	};
+    	victim = new Client(server.accept());
+    	System.out.println("Victim connected.");
+    	assasin = new Client(server.accept());
+    	System.out.println("Assassin connected.");
     	
-    	Timer timer = new Timer(30000, actionListener);
-    	timer.start();
-    	
-    	/*int time = 0;
-    	while (time < max) {
-			Socket client = server.accept();
-			PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-			System.out.println(client);
-    	}*/
-    	
-    	while (true) {
+    	while (true)
+    	{
     		
 		}
     }
     
-    public static double angleFinder(double targetX, double targetY ){
-		double theta, angle;
-		theta = Math.atan2(targetY , targetX);
-		theta += Math.PI/360;
-		angle = Math.toDegrees(theta);
-		if (angle < 0) {
-	        angle += 360;
-	    }
-		return angle;
-	}
-	
-	public static double targetDistance(double targetX, double targetY ){
-		double distance;
-		distance = Math.sqrt((targetX * targetX) + (targetY * targetY));
-		return distance;
-	}
-	
-	public static int[][] data;
-	
-	public static void gameData(int playerCount){
-		ArrayList<Integer> randomizer = new ArrayList<Integer>();
-		int[][] data = new int[playerCount][2];
-		for(int i = 0; i < playerCount; i++){
-			randomizer.add(new Integer(i+1));
-		}
-	
-		Collections.shuffle(randomizer);
-		
-		for(int i = 0; i < playerCount; i++){
-			data[i][0] = i+1;
-			data[i][1] = randomizer.get(i);
-		}
-		
-	}
 	
 }
