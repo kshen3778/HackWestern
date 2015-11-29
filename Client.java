07f034fbc858eb69e37cbe71c804b15c43637bb0
Kevin S <kshen3778@gmail.com>
2015-11-29 08:43:05 +0000

    update

--- Client.java
+++ Client.java
@@ -7,44 +7,12 @@ public class Client
     private Socket socket;
     private PrintWriter out;
     private BufferedReader in;
-    private double latitude;
-    private double longitude;
-    private int health;
-    private int damage;
     
     public Client(Socket s) throws IOException
     {
         socket = s;
 		out = new PrintWriter(s.getOutputStream(), true);
 		in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-		System.out.println("client..."); // TODO DEBUG
-		give("latitude");
-		latitude = Double.parseDouble(get());
-		System.out.println("latitude: " + latitude); // TODO DEBUG
-		give("longitude");
-		longitude = Double.parseDouble(get());
-		System.out.println("longitude: " + longitude); // TODO DEBUG
-		health = 100;
-		damage = 20;
-    }
-    
-    public Client(Socket s, boolean b) throws IOException
-    {
-    	if (b) {
-	        socket = s;
-			out = new PrintWriter(s.getOutputStream(), true);
-			in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-			give("latitude");
-			latitude = Double.parseDouble(get());
-			give("longitude");
-			longitude = Double.parseDouble(get());
-			health = 100;
-			damage = 20;
-    	} else {
-	        socket = s;
-			out = new PrintWriter(s.getOutputStream(), true);
-			in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-    	}
     }
     
     public void give(String str) throws IOException
@@ -57,19 +25,7 @@ public class Client
         return in.readLine();
     }
     
-    public void takeDamage(){
-    	health -= damage;
-    }
-    
-    public void subtractDamage(int n){
-    	damage -= n;
-    }
-    
-    public int getHealth(){
-    	return health;
-    }
-    
-    public double angleFinder(double targetX, double targetY ){
+    public double angleFinder(double targetX, double targetY) {
 		double theta, angle;
 		theta = Math.atan2(targetY , targetX);
 		theta += Math.PI/360;
@@ -78,44 +34,6 @@ public class Client
 	        angle += 360;
 	    }
 		return angle;
-	}
-	
-	public double targetDistance(double targetX, double targetY ){
-		double distance;
-		distance = Math.sqrt((targetX * targetX) + (targetY * targetY));
-		return distance;
-	}
-	
-	public double getLongitude(){
-	    return longitude;
-	}
-	
-	public double getLatitude(){
-	    return latitude;
-	}
-    
-    public int heatTone(double lat, double lon ){
-		double distance = 0;
-		int heat = 0;
-		distance = Math.sqrt(((lat - latitude) * (lat - latitude)) + ((lon - longitude) * (lon - longitude)));
-		if(distance <= 1){
-			heat = 5;
-		}else if (distance <= 2){
-			heat = 4;
-		}else if (distance <= 4){
-			heat = 3;
-		}else if (distance <= 8){
-			heat = 2;
-		}else if (distance <= 16){
-			heat = 1;
-		}else{
-			heat = 0;
-		}
-		return heat;
-	}
-	
-	public boolean isStabbing() throws IOException{
-		return get().equals("true");
 	}
 	
 }
--- Server.java
+++ Server.java
@@ -23,85 +23,63 @@ public class Server
     
     public void listen() throws IOException
     {
-    	Client assassin;
-    	Client victim;
-    	double assAngle, vicAngle, distance, heatTone;
-    	
-    	(new Client(server.accept(), false)).give("victim");
+    	(new Client(server.accept())).give("victim");
     	System.out.println("Victim found.");
-    	victim = new Client(server.accept());
+    	Client victim = new Client(server.accept());
     	System.out.println("Victim connected.");
-    	(new Client(server.accept(), false)).give("assassin");
+    	(new Client(server.accept())).give("assassin");
     	System.out.println("Assassin found.");
-    	assassin = new Client(server.accept());
+    	Client assassin = new Client(server.accept());
     	System.out.println("Assassin connected.");
-    	
-    	//public static int[][] data;
-    	//public void gameData(int playerCount){
-		//	data = new int[playerCount][2];
-		//	for(int i = 0; i < playerCount; i++){
-		//		data[i][0] = i+1;
-		//		data[i][1] = i;
-		//	}
-		//	data[0][1] = playerCount;
-		//}
-	
     	
     	while (true)
     	{
-    		
-    		
     		victim.give("ping");
-    		assassin.give("ping");
-    		
-    		//debug statements
-    		System.out.println(victim.get());
-    		System.out.println(assassin.get());
-    		
-    		if(victim.get() == null){
+    		if (victim.get() == null) {
     			System.out.println("Victim disconnected.");
     			break;
     		}
     		
-    		if(assassin.get() == null){
+    		assassin.give("ping");
+    		if (assassin.get() == null) {
     			System.out.println("Assassin disconnected.");
     			break;
     		}
 			
     		assAngle = assassin.angleFinder(victim.getLatitude(),victim.getLongitude());
-    		vicAngle = victim.angleFinder(assassin.getLatitude(),assassin.getLongitude());
-    		distance = assassin.targetDistance(victim.getLatitude() - assassin.getLatitude(), victim.getLongitude()-assassin.getLongitude());
-    		heatTone = assassin.heatTone(victim.getLatitude(), victim.getLongitude()); 
+    		assassin.give("angle " + assAngle);
     		
-    		
-    		assassin.give("assAngle" + assAngle);
-    		//victim.give("vicAngle" + vicAngle);
-    		assassin.give("distance" + distance);
-    		assassin.give("heatTone" + heatTone);
-    		
-    		
-    		
-    		if(victim.getHealth() <= 0){
-    			System.out.println("Assassin has slain the victim. Well played.");
- 				assassin.give("Assassin Wins!");
- 				break;
-    		}
-    		
- 			if(distance <= 1 && assassin.isStabbing()) {
-	 			victim.takeDamage();
-	 			victim.give("Health: " + victim.getHealth());
-	 			//1 second "recovery" time
-	 			try {
-	    			Thread.sleep(1000);
-				} catch(InterruptedException ex) {
-	    			Thread.currentThread().interrupt();
-				}
- 			}else if(distance > 1 && assassin.isStabbing()){
- 				assassin.subtractDamage(2);
- 			}
+    		victim.give("latitude");
+			double vicLat = Double.parseDouble(victim.get());
+			victim.give("longitude");
+			double vicLng = Double.parseDouble(victim.get());
+    		assassin.give("latitude");
+			double assLat = Double.parseDouble(assassin.get());
+			assassin.give("longitude");
+			double assLng = Double.parseDouble(assassin.get());
+			
+			if (distFrom(vicLat, vicLng, assLat, assLng) < 2)
+				victim.give("hurt");
+			
+			victim.give("health");
+			if (Integer.parseInt(victim.get()) == 0)
+				break;
 		}
+		
+		System.out.println("Assassin wins!");
     }
     
-
+	public static float distFrom(float lat1, float lng1, float lat2, float lng2) {
+	    double earthRadius = 6371000; //meters
+	    double dLat = Math.toRadians(lat2-lat1);
+	    double dLng = Math.toRadians(lng2-lng1);
+	    double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+	               Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+	               Math.sin(dLng/2) * Math.sin(dLng/2);
+	    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+	    float dist = (float) (earthRadius * c);
 	
+	    return dist;
+    }
+
 }
